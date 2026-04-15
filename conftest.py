3bf6090d79031b75e18bdcd75d5b0fce6e6a1773
SRALICK <genasamoxod@gmail.com>
2026-04-15 14:17:03 +0300

    feat: add github actions workflow

--- conftest.py
+++ conftest.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support import expected_conditions as ES
 from selenium.webdriver.firefox.options import Options
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.firefox.service import Service as FirefoxService
+import time
 
 @pytest.fixture(scope="session")
 def auth_token():
@@ -22,9 +23,13 @@ def auth_token():
 
 @pytest.fixture
 def browser_logged_in(auth_token):
+    # path = r"C:\Users\genas\.wdm\drivers\geckodriver\win64\v0.36.0\geckodriver.exe"
+    # service = Service(executable_path=path)
     service = FirefoxService(GeckoDriverManager().install())
     options = Options()
     options.add_argument('--headless')
+    options.add_argument('--width=1920')
+    options.add_argument('--height=1080')
     options.page_load_strategy = 'eager'
     driver = webdriver.Firefox(service=service, options = options)
     driver.get(os.getenv('BASE_URL')) # Сначала заходим на сайт
@@ -33,9 +38,12 @@ def browser_logged_in(auth_token):
     # Большинство современных сайтов (как Conduit) хранят токен там
     script = f"window.localStorage.setItem('id_token', '{auth_token}');"
     driver.execute_script(script)
-    
+
+    time.sleep(2)
+
     # Обновляем страницу — и вуаля, ты залогинен!
     driver.refresh()
     
+    time.sleep(2)
     yield driver
     driver.quit()
--- tests/test_ui/test_mark_name.py
+++ tests/test_ui/test_mark_name.py
@@ -6,6 +6,6 @@ import time
 
 def test_user_is_logged_in(browser_logged_in):
     wait = WebDriverWait(browser_logged_in,10)
-    profile_link = wait.until(ES.visibility_of_element_located((By.XPATH, '//a[@href="/@valerick/"]')))
+    profile_link = wait.until(ES.visibility_of_element_located((By.XPATH, '//a[contains(@href, "/@")]')))
     assert profile_link.is_displayed()
     print(f"Успешно залогинены под: {profile_link.text}")
